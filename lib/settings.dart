047742d53d3f48a24ca7237e2cf64e2ef067df30
lachlan.b.wright@student.uts.edu.au <lachlan.b.wright@student.uts.edu.au>
2022-08-08 23:57:08 +1000

    Added btn

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class SettingsScreen extends StatefulWidget {
@@ -8,10 +10,36 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  final keyController = TextEditingController();
+
+  @override
+  void dispose() {
+    //This is akin to ReactJS' componentWillUnmount()
+    keyController.dispose();
+    super.dispose(); //Calls parent's dispose function
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text('Settings')),
-        body: const Align(alignment: Alignment.topCenter, child: Text('TEST')));
+        body: Align(
+            alignment: Alignment.topCenter,
+            child: Column(
+              children: [
+                const SizedBox(height: 10),
+                TextField(
+                  controller: keyController,
+                  style: 
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(50)),
+                  child: const Text('Set API Key'),
+                ),
+              ],
+            )));
   }
 }
