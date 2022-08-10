0ade6a014e3a01d3501e0a8e89b087a622922a95
lachlan.b.wright@student.uts.edu.au <lachlan.b.wright@student.uts.edu.au>
2022-08-11 01:57:59 +1000

    Added api key storage

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({Key? key}) : super(key: key);
@@ -11,11 +12,13 @@ class SettingsScreen extends StatefulWidget {
 
 class _SettingsScreenState extends State<SettingsScreen> {
   final keyController = TextEditingController();
+  String _apiKey = "";
 
   //This is akin to ReactJS' componentDidMount()
   @override
   void initState() {
     super.initState();
+    _loadApiKey();
   }
 
   //This is akin to ReactJS' componentWillUnmount()
@@ -23,6 +26,21 @@ class _SettingsScreenState extends State<SettingsScreen> {
   void dispose() {
     keyController.dispose();
     super.dispose(); //Calls parent's dispose function
+  }
+
+  Future<void> _loadApiKey() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _apiKey = prefs.getString('apiKey') ?? "";
+    });
+  }
+
+  Future<void> _setApiKey() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _apiKey = keyController.text;
+      prefs.setString('apiKey', keyController.text);
+    });
   }
 
   @override
@@ -42,15 +60,15 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 ),
                 const SizedBox(height: 10),
                 ElevatedButton(
-                  onPressed: () {
-                    //print(keyController.text);
-                  },
+                  onPressed: _setApiKey,
                   style: ElevatedButton.styleFrom(
                       minimumSize: const Size.fromHeight(50)),
                   child: const Text('Set API Key'),
                 ),
                 const SizedBox(height: 10),
-                const Text('Your API Key is: ')
+                Text(_apiKey == ""
+                    ? 'Your API key has not been set'
+                    : 'Your API Key is: $_apiKey')
               ],
             )));
   }
