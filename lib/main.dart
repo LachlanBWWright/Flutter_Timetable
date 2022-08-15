70205af2374f29754fc2f56cfc4680ecb4f22c85
lachlan.b.wright@student.uts.edu.au <lachlan.b.wright@student.uts.edu.au>
2022-08-16 02:29:12 +1000

    Added Station Selection and deletion

--- lib/main.dart
+++ lib/main.dart
@@ -31,14 +31,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -66,22 +58,13 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+          children: const <Widget>[
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              'Placeholder text',
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- lib/new_trip.dart
+++ lib/new_trip.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'dart:io';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter/material.dart';
@@ -14,24 +16,29 @@ class NewTripScreen extends StatefulWidget {
 }
 
 class _NewTripScreenState extends State<NewTripScreen> {
+  List<Station> _stationList = [];
+  String _firstStation = '';
+  String _secondStation = '';
+
+  void setStation(String station) {
+    setState(() {
+      if (_firstStation == '') {
+        _firstStation = station;
+      } else {
+        _secondStation = station;
+      }
+
+      print('Stations: $_firstStation, $_secondStation');
+    });
+  }
+
   Future<http.Response> fetchTrips() async {
     final prefs = await SharedPreferences.getInstance();
     final apiKey = prefs.getString('apiKey');
 
-    /* $params = array(
-    'outputFormat' => 'rapidJSON',
-    'odvSugMacro' => 1
-    'name_sf' => $searchQuery,
-    'coordOutputFormat' => 'EPSG:4326',
-    'TfNSWSF' => 'true'
-    ); */
-
-    //https://opendata.transport.nsw.gov.au/system/files/resources/Trip%20Planner%20API%20manual-opendataproduction%20v3.2.pdf
-    //https://opendata.transport.nsw.gov.au/node/601/exploreapi#!/default/tfnsw_stopfinder_request
-
     final params = {
+      //https://opendata.transport.nsw.gov.au/system/files/resources/Trip%20Planner%20API%20manual-opendataproduction%20v3.2.pdf https://opendata.transport.nsw.gov.au/node/601/exploreapi#!/default/tfnsw_stopfinder_request
       'outputFormat': 'rapidJSON',
-      //'odvSugMacro': '1',
       'type_sf': 'stop',
       'name_sf': 'a',
       'coordOutputFormat': 'EPSG:4326',
@@ -40,14 +47,8 @@ class _NewTripScreenState extends State<NewTripScreen> {
     };
     final uri =
         Uri.https('api.transport.nsw.gov.au', '/v1/tp/stop_finder/', params);
-
     final res =
         await http.get(uri, headers: {'authorization': 'apikey $apiKey'});
-
-    print(res.body);
-
-    //print(body.length);
-
     return http.get(
       Uri.parse(
           'https://api.transport.nsw.gov.au/v1/publictransport/timetables/complete'),
@@ -55,29 +56,143 @@ class _NewTripScreenState extends State<NewTripScreen> {
     );
   }
 
-  void loadStations() async {
-    final _dataset =
+  Future<List<Station>> loadStations() async {
+    final dataset =
         await rootBundle.loadString('assets/LocationFacilityData.csv');
+    List<dynamic> data = const CsvToListConverter().convert(dataset, eol: "\n");
+    data.removeWhere((station) => !station[0].contains("Station"));
+
+    var stations = List<Station>.empty(growable: true);
+
+    for (var element in data) {
+      stations.add(Station(element[0], element[6]));
+    }
+
+    print(stations[57]);
+    return stations;
   }
 
   @override
   void initState() {
     super.initState();
-
-    loadStations();
-
-    fetchTrips();
+    loadStations().then((res) {
+      setState(() {
+        {
+          _stationList = res;
+        }
+      });
+    });
+    //fetchTrips();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Add New Trip')),
-        body: Align(
-            alignment: Alignment.topCenter,
-            child: Column(children: [
-              const Text('MALARKEY'),
-              const SizedBox(height: 10),
-            ])));
+        appBar: AppBar(
+          title: const Text('Add New Trip'),
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.search))
+          ],
+        ),
+        body: Column(
+          children: [
+            if (_firstStation != '')
+              Stack(
+                children: [
+                  Align(
+                    child: Text(
+                      'First Station Selected: $_firstStation',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Positioned(
+                      right: 0,
+                      child: InkWell(
+                          child: Icon(Icons.cancel, size: 20),
+                          onTap: (() {
+                            setState(() {
+                              _firstStation = '';
+                            });
+                          })))
+                ],
+              ),
+            if (_secondStation != '')
+              Stack(
+                children: [
+                  Align(
+                    child: Text(
+                      'Second Station Selected: $_secondStation',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Positioned(
+                      right: 0,
+                      child: InkWell(
+                          child: Icon(Icons.cancel, size: 20),
+                          onTap: (() {
+                            setState(() {
+                              _secondStation = '';
+                            });
+                          })))
+                ],
+              ),
+            Expanded(
+              child: StationList(
+                listItems: _stationList,
+                setStation: setStation,
+              ),
+            )
+          ],
+        ));
+  }
+}
+
+class Station {
+  String name;
+  String address;
+  Station(this.name, this.address);
+}
+
+class StationList extends StatelessWidget {
+  final List<Station> listItems;
+  final Function(String) setStation;
+
+  const StationList(
+      {Key? key, required this.listItems, required this.setStation})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: listItems.length,
+        itemBuilder: (context, index) {
+          return StationView(station: listItems[index], setStation: setStation);
+        });
+  }
+}
+
+class StationView extends StatelessWidget {
+  final Station station;
+  final Function(String) setStation;
+
+  const StationView({Key? key, required this.station, required this.setStation})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(1.0),
+      child: Card(
+          child: InkWell(
+              onTap: () {
+                setStation(station.name);
+              },
+              child: Column(
+                children: [
+                  Text(station.name),
+                  Text(station.address),
+                ],
+              ))),
+    );
   }
 }
